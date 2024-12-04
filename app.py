c85f6dd58e238dd5d376a7d8f7a4f92f840acec8
Dale Nunns <dale@stuff.za.net>
2024-12-04 19:42:28 +0200

    More WIP

--- app.py
+++ app.py
@@ -47,3 +47,16 @@ def index():
 @app.route('/control')
 def index():
     return send_file('static/control.html')
+
+
+# --- Event Queue ---
+@app.route('/listen', methods=['GET'])
+def listen():
+
+    def stream():
+        messages = announcer.listen()  # returns a queue.Queue
+        while True:
+            msg = messages.get()  # blocks until a new message arrives
+            yield msg
+
+    return flask.Response(stream(), mimetype='text/event-stream')
